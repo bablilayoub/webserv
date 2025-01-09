d7cab95556dc7160a544ba9e9b7840b1a6b725e4
Redx-at <taoussi.aimen@gmail.com>
2025-01-09 14:42:47 +0100

    Add folder for work

---
+++ file_uploads/FileUpload.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FileUpload.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aitaouss <aitaouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 14:34:45 by aitaouss          #+#    #+#             */
+/*   Updated: 2025/01/09 14:41:07 by aitaouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FileUpload.hpp"
+
+FileUpload::FileUpload() : name("aimen") {
+    std::cout << "Default constructor Called" << std::endl;
+}
---
+++ file_uploads/FileUpload.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fileUpload.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aitaouss <aitaouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 14:33:18 by aitaouss          #+#    #+#             */
+/*   Updated: 2025/01/09 14:40:58 by aitaouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string.h>
+
+class   FileUpload {
+    public:
+        std::string name;
+        FileUpload();
+};
