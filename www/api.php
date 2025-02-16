91d62c90256f1f7f11f27c4e372a5b85cc04169b
bablilayoub <ayoubbablil@gmail.com>
2025-02-16 19:00:11 +0100

    Improved some things

--- www/api.php
+++ www/api.php
@@ -16,30 +16,44 @@ if (!file_exists($targetDir)) {
 	exit;
 }
 
-$filename = isset($_GET["filename"]) ? $_GET["filename"] : "";
+$path = isset($_GET["path"]) ? $_GET["path"] : "";
 
-$filename = basename($filename);
-
-if (empty($filename)) {
+if (empty($path)) {
 	http_response_code(400);
-	echo json_encode(array("message" => "Please enter a file name"));
+	echo json_encode(array("message" => "Please enter a valid path"));
 	exit;
 }
 
-$filePath = realpath($targetDir . "/" . $filename);
+$full_path = realpath($targetDir . "/" . $path);
 
-if (!file_exists($filePath)) {
+if (!file_exists(filename: $full_path)) {
 	http_response_code(404);
-	echo json_encode(array("message" => "File not found"));
+	echo json_encode(array("message" => "File or directory does not exist"));
 	exit;
 }
 
-if (unlink($filePath)) {
-	http_response_code(200);
-	echo json_encode(array("message" => "File deleted successfully"));
+if (strpos($full_path, $targetDir) !== 0) {
+	http_response_code(403);
+	echo json_encode(array("message" => "Forbidden"));
+	exit;
+}
+
+if (is_dir($full_path)) {
+	if (rmdir($full_path)) {
+		http_response_code(200);
+		echo json_encode(array("message" => "Directory deleted successfully"));
+	} else {
+		http_response_code(500);
+		echo json_encode(array("message" => "Failed to delete directory"));
+	}
 } else {
-	http_response_code(500);
-	echo json_encode(array("message" => "Failed to delete the file"));
+	if (unlink($full_path)) {
+		http_response_code(200);
+		echo json_encode(array("message" => "File deleted successfully"));
+	} else {
+		http_response_code(500);
+		echo json_encode(array("message" => "Failed to delete file"));
+	}
 }
 
 ?>
